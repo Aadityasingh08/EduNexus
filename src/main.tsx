5b16d97a5a891be27885785123db759728fdb370
Sahayta User <sahayta@project.local>
2026-09-19 19:55:55 +0530

    Trigger final Vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+console.log("EduNexus v2.0 - Autonomous AI Engine Loaded");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
